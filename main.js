f70bf308aec9cfe4ede94d82af8a69b7581f911f
Noor Fatima <160162835+NoorFatima7654@users.noreply.github.com>
2024-03-04 05:23:10 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+//Question no 7
+// console.log(5+3);
+// console.log(10-2);
+// console.log(4*2);
+// console.log(16/2);
+//Question no 8
+// console.log(5+3);
+// console.log(10-2);
+// console.log(4*2);
+// console.log(16/2);
+//Question no 9
+var favourite_number = 12;
+console.log("My Favourite number is ".concat(favourite_number));
---
+++ main.ts
@@ -0,0 +1,21 @@
+//Question no 7
+console.log(5+3);
+
+console.log(10-2);
+
+console.log(4*2);
+
+console.log(16/2);
+
+//Question no 8
+console.log(5+3);
+
+console.log(10-2);
+
+console.log(4*2);
+
+console.log(16/2);
+
+//Question no 9
+let favourite_number: number= 12;
+console.log(`My Favourite number is ${favourite_number}`)
